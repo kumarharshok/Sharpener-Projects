de0cf94701db8753af4f84540739d55efd04e260
Harsh Kumar <167678135+kumarharshok@users.noreply.github.com>
2026-04-05 16:43:20 +0530

    Implement user middleware and welcome route
    Added user middleware to set default user and welcome route.

--- Node.js/Express/CreateServer/app.js
+++ Node.js/Express/CreateServer/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
-
-
-
-
+const userMiddleware = (req, res, next) => {
+    req.user = "Guest"
+    next();
+}
+app.get("/welcome",userMiddleware, (req, res) => {
+    res.send(`<h1>Welcome ${req.user}!</h1>` )
+});
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
