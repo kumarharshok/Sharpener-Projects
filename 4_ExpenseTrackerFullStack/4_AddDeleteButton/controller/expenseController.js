2721b44bc9c59fcc16e02db9a6257805b2b069e4
Harsh Kumar <167678135+kumarharshok@users.noreply.github.com>
2026-04-11 04:49:36 +0530

    Add files via upload

---
+++ 4_ExpenseTrackerFullStack/4_AddDeleteButton/controller/expenseController.js
@@ -0,0 +1,54 @@
+const { use } = require('react');
+const expenseTable = require('../model/dashboard');
+
+const addExpense = async (req, res) => {
+    const { amount, description, category, userId } = req.body;
+    try {
+        if(userId === null) {
+            res.status(202).json({message: "Session expired!"})
+            return;
+        }
+        await expenseTable.create({amount, description, category, userId});
+
+        res.status(200).json({message: "Expense Added!"})
+    } catch(error) {
+        res.status(400).json({message: "Something went wrong!"})
+    }
+}
+const getAllExpenses = async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const data = await expenseTable.findAll({where: {userId: userId}});
+
+        if(!data) {
+            res.status(202).json({message: "No recorde found!"})
+            return;
+        }
+
+        res.status(200).json({data: data});
+    } catch (error) {
+        res.status(500).send({message: "Something went wrong!"});
+    }
+
+}
+
+const deleteExpense = async (req, res) => {
+
+    const expsenseId = req.params.id;
+
+    try {
+        await expenseTable.destroy({where: {id : expsenseId}});
+
+        res.status(200).json({message: "item removed successfully refresh the page!"})
+    } catch(error) {
+        res.status(400).json({message: "something went wrong!"});
+    }
+
+}
+
+module.exports = {
+    addExpense,
+    getAllExpenses,
+    deleteExpense
+}
---
+++ 4_ExpenseTrackerFullStack/4_AddDeleteButton/model/user.js
@@ -0,0 +1,27 @@
+const sequelize = require('../config/db');
+const { DataTypes } = require('sequelize');
+
+const user = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'user',
+    timestamps: false
+})
+
+module.exports = user;
