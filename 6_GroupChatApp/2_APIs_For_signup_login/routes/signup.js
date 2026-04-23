d44d6b127ef88b5cbd6ebded496c5c7433fdfb43
Harsh Kumar <167678135+kumarharshok@users.noreply.github.com>
2026-04-23 13:53:39 +0530

    Add files via upload

---
+++ 6_GroupChatApp/2_APIs_For_signup_login/routes/signup.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const signupController = require('../controller/signup');
+
+router.get('/', signupController.showForm);
+router.post('/signup', signupController.addUser);
+
+
+
+module.exports = router;
